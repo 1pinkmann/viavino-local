509d3170f138b2a35b986dcaf74a4500beb87366
Pinkmann <korgov.vova@icloud.com>
2021-04-28 18:58:56 +0300

    final

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $version             = '1.0.2';
 // $base_url            = in_array( $_SERVER['REMOTE_ADDR'], [ '127.0.0.1', '::1' ] ) ? 'http://viavino/viavino-html' : 'https://viavino.it';
-$base_url            = in_array( $_SERVER['REMOTE_ADDR'], [ '127.0.0.1', '::1' ] ) ? 'http://viavino/' : 'https://viavino.it';
+$base_url            = in_array( $_SERVER['REMOTE_ADDR'], [ '127.0.0.1', '::1' ] ) ? 'http://viavino/' : 'https://viavino.preview.ee/';
 $available_languages = [ 'en', 'it' ];
 $default_language    = current( $available_languages );
 
